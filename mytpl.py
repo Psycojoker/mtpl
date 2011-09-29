8c843db31c68fc5dee58f4802d417bd30e1f42c2
Laurent Peuch <cortex@worlddomination.be>
2011-09-29 11:22:45 +0200

    [enh] clone my templates git repository into the correct dir

--- mytpl.py
+++ mytpl.py
@@ -12,6 +12,9 @@ def init():
         os.mkdir(os.path.expanduser("~/.config/mytemplates/"))
     if os.system("cd ~/.config/mytemplates/ && git status > /dev/null 2>&1") != 0:
         print "Cloning templates in %s" % os.path.expanduser("~/.config/mytemplates/")
+        if os.system("git clone git://github.com/Psycojoker/mytemplates.git ~/.config/mytemplates/") != 0:
+            print sys.stderr, "Didn't managed to clone git repository in %s" % os.path.expanduser("~/.config/mytemplates/")
+            print sys.stderr, "Abort"
 
 if __name__ == "__main__":
     init()
