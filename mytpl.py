3848b9cd55afb4d28fe3e8879a0e66d2e9de7e78
Laurent Peuch <cortex@worlddomination.be>
2011-09-29 10:51:18 +0200

    [enh] check if template dir exist

--- mytpl.py
+++ mytpl.py
@@ -7,6 +7,9 @@ def init():
     if os.system("which 'git' > /dev/null") != 0:
         print >>sys.stderr, "Can't managed to find git, abort"
         sys.exit(1)
+    if not os.path.exists(os.path.expanduser("~/.config/mytemplates/")):
+        print "Creating templates dir in %s" % os.path.expanduser("~/.config/mytemplates/")
+        os.mkdir(os.path.expanduser("~/.config/mytemplates/"))
 
 if __name__ == "__main__":
     init()
