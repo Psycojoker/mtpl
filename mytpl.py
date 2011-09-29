59ce2f255b6e58c1b1a22ae0f9f44079068cee88
Laurent Peuch <cortex@worlddomination.be>
2011-09-29 10:51:36 +0200

    [enh] check if template dir is a git dir

--- mytpl.py
+++ mytpl.py
@@ -10,6 +10,8 @@ def init():
     if not os.path.exists(os.path.expanduser("~/.config/mytemplates/")):
         print "Creating templates dir in %s" % os.path.expanduser("~/.config/mytemplates/")
         os.mkdir(os.path.expanduser("~/.config/mytemplates/"))
+    if os.system("cd ~/.config/mytemplates/ && git status > /dev/null 2>&1") != 0:
+        print "Cloning templates in %s" % os.path.expanduser("~/.config/mytemplates/")
 
 if __name__ == "__main__":
     init()
