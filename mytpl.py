504f00b478f1c0ddfccd18a9e7363d8f624408ef
Laurent Peuch <cortex@worlddomination.be>
2011-09-29 11:39:03 +0200

    [mod] refactoring, use a constante for the dir path

--- mytpl.py
+++ mytpl.py
@@ -3,17 +3,19 @@
 import os
 import sys
 
+DIR = os.path.expanduser("~/.config/mytemplates/")
+
 def init():
     if os.system("which 'git' > /dev/null") != 0:
         print >>sys.stderr, "Can't managed to find git, abort"
         sys.exit(1)
-    if not os.path.exists(os.path.expanduser("~/.config/mytemplates/")):
-        print "Creating templates dir in %s" % os.path.expanduser("~/.config/mytemplates/")
-        os.mkdir(os.path.expanduser("~/.config/mytemplates/"))
-    if os.system("cd ~/.config/mytemplates/ && git status > /dev/null 2>&1") != 0:
-        print "Cloning templates in %s" % os.path.expanduser("~/.config/mytemplates/")
-        if os.system("git clone git://github.com/Psycojoker/mytemplates.git ~/.config/mytemplates/") != 0:
-            print sys.stderr, "Didn't managed to clone git repository in %s" % os.path.expanduser("~/.config/mytemplates/")
+    if not os.path.exists(DIR):
+        print "Creating templates dir in %s" % DIR
+        os.mkdir(DIR)
+    if os.system("cd %s && git status > /dev/null 2>&1" % DIR) != 0:
+        print "Cloning templates in %s" % DIR
+        if os.system("git clone git://github.com/Psycojoker/mytemplates.git %s" % DIR) != 0:
+            print sys.stderr, "Didn't managed to clone git repository in %s" % DIR
             print sys.stderr, "Abort"
 
 if __name__ == "__main__":
