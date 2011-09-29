8bb63921d887a53d54181fee3d949fb373c86b82
Laurent Peuch <cortex@worlddomination.be>
2011-09-29 10:51:03 +0200

    [enh] check if git exist

--- mytpl.py
+++ mytpl.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
 
+import os
+import sys
+
+def init():
+    if os.system("which 'git' > /dev/null") != 0:
+        print >>sys.stderr, "Can't managed to find git, abort"
+        sys.exit(1)
+
 if __name__ == "__main__":
-    pass
+    init()
