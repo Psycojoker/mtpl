c9f3f43f4f9fde198b4e7085063688e655ee5e8a
Laurent Peuch <cortex@worlddomination.be>
2011-10-06 11:17:42 +0200

    [enh] add a setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='MyTemplates',
+      version='0.1',
+      description='scripts templates manager that use git',
+      author='Laurent Peuch',
+      # long_description=open("README.md").read(),
+      author_email='cortex@worlddomination.be',
+      url='http://worlddomination.be/projects/mtpl',
+      license= 'GPLv3+',
+      # list of scripts supplied by your application
+      scripts=['mtpl', 'mytemplates'],
+      keywords='templates script cli git',
+     )
+
+# vim:set shiftwidth=4 tabstop=4 expandtab:
